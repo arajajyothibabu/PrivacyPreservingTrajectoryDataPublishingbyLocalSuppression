c38553bb1b2e0768ec541318fa04b84a7cd7051e
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-15 00:35:03 +0530

    DataBase connection object created

---
+++ src/utils/DB.java
@@ -0,0 +1,39 @@
+package utils;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * Created by Araja Jyothi Babu on 15-Apr-16.
+ */
+public class DB {
+
+    private static String DBURL = "jdbc:oracle:thin:@localhost:1521:XE";
+    private static String DBUSER = "system";
+    private static String DBPASS = "tiger";
+    private static Connection connection;
+
+    public DB() {
+    }
+
+    public static Connection openConnection() throws Exception {
+
+        Class.forName("oracle.jdbc.driver.OracleDriver");
+
+        connection = DriverManager.getConnection(DBURL, DBUSER, DBPASS );
+
+        return connection;
+
+    }
+
+    public static void closeConnection() throws Exception {
+        connection.close();
+    }
+
+    public static Boolean commitDB() throws Exception {
+        Statement commitStatement = connection.createStatement();
+        return commitStatement.execute("COMMIT");
+    }
+
+}
