c53e00fbc40dccdeb86eeb1a262ed24bc92ce3bb
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-17 00:05:01 +0530

    sensitiveDataService updated

--- src/services/SensitiveDataService.java
+++ src/services/SensitiveDataService.java
@@ -1,8 +1,26 @@
 package services;
 
+import models.SensitiveDataModel;
+import utils.DB;
+import utils.OracleDAO;
+
 /**
  * Created by Araja Jyothi Babu on 15-Apr-16.
  */
 public class SensitiveDataService {
     //TODO:
+
+    DB db;
+    OracleDAO dao;
+
+    public SensitiveDataService(DB db) throws Exception{
+        this.db = db;
+        this.dao = new OracleDAO(db);
+    }
+
+    public SensitiveDataModel getSensitiveData(int id) throws Exception {
+        SensitiveDataModel sensitiveDataModel = dao.getSensitiveData(id);
+        return sensitiveDataModel;
+    }
+
 }
