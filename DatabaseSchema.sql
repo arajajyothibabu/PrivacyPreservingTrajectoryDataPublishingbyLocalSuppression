264fa7fdb183eb742d21be4d507f6dd322c9170a
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-16 21:14:43 +0530

    database schema updated

---
+++ DatabaseSchema.sql
@@ -0,0 +1,36 @@
+CREATE TABLE rawdatatable (
+  id NUMBER(10) NOT NULL PRIMARY KEY,
+  path VARCHAR2(100),
+  diagnosis VARCHAR2(30)
+);
+
+CREATE TABLE trajectorydata (
+  id NUMBER(10) NOT NULL,
+  location VARCHAR2(10) NOT NULL,
+  time VARCHAR2(10) NOT NULL
+  REFERENCES rawdatatable
+);
+
+CREATE TABLE sensitivedata (
+  id NUMBER(10) NOT NULL PRIMARY KEY ,
+  diagnosis VARCHAR2(30)
+  REFERENCES rawdatatable
+);
+
+INSERT INTO trajectorydata VALUES('1', 'a', '1');
+INSERT INTO trajectorydata VALUES('1', 'd', '2');
+INSERT INTO trajectorydata VALUES('1', 'b', '3');
+INSERT INTO trajectorydata VALUES('1', 'e', '4');
+INSERT INTO trajectorydata VALUES('1', 'f', '6');
+INSERT INTO trajectorydata VALUES('1', 'e', '8');
+INSERT INTO trajectorydata VALUES('2', 'd', '2');
+INSERT INTO trajectorydata VALUES('2', 'c', '5');
+INSERT INTO trajectorydata VALUES('2', 'f', '6');
+INSERT INTO trajectorydata VALUES('2', 'c', '7');
+INSERT INTO trajectorydata VALUES('2', 'e', '8');
+
+INSERT INTO sensitivedata VALUES('1', 'HIV');
+INSERT INTO sensitivedata VALUES('2', 'FEVER');
+INSERT INTO sensitivedata VALUES('3', 'Hepatitis');
+INSERT INTO sensitivedata VALUES('4', 'Flu');
+INSERT INTO sensitivedata VALUES('5', 'HIV');
