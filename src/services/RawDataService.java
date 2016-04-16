0ee3f3ebdd1508ce8159991c8a09f07fab4e8283
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-16 23:50:20 +0530

    rawDataService updated

--- src/services/RawDataService.java
+++ src/services/RawDataService.java
@@ -1,9 +1,42 @@
 package services;
 
+import models.RawDataModel;
+import utils.DB;
+import utils.OracleDAO;
+
+import java.util.ArrayList;
+
 /**
  * Created by Araja Jyothi Babu on 15-Apr-16.
  */
 public class RawDataService {
 
-    //TODO:
+    DB db;
+    OracleDAO dao;
+
+    public RawDataService(DB db) throws Exception{
+        this.db = db;
+        this.dao = new OracleDAO(db);
+    }
+
+    public ArrayList<RawDataModel> getRawData() throws Exception{
+        ArrayList<RawDataModel> rawDataModels = dao.getRawData();
+        return rawDataModels;
+    }
+
+    public RawDataModel getRawData(int id) throws Exception{
+        RawDataModel rawData = dao.getRawData(id);
+        return rawData;
+    }
+
+    public boolean insertRawData(RawDataModel rawDataModel) throws Exception {
+        boolean isInserted = dao.insertRawData(rawDataModel);
+        return isInserted;
+    }
+
+    public boolean insertRawData(ArrayList<RawDataModel> rawDataModels) throws Exception {
+        boolean isInserted = dao.insertRawData(rawDataModels);
+        return isInserted;
+    }
+
 }
