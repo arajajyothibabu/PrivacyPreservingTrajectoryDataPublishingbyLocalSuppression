560946c8eb1ba343cb08e02d9daf77372bb10a22
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-16 22:01:33 +0530

    sensitivedata model updated

--- src/models/SensitiveDataModel.java
+++ src/models/SensitiveDataModel.java
@@ -6,10 +6,20 @@ package models;
 public class SensitiveDataModel {
 
     //TODO: have to implement if required
+    public int id;
     public String diagnosis;
 
-    public SensitiveDataModel(String diagnosis) {
+    public SensitiveDataModel(int id, String diagnosis) {
+        this.id = id;
         this.diagnosis = diagnosis;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
     }
 
     public String getDiagnosis() {
