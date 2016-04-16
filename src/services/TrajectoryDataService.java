f24e1442bbdf33f2d2347fe19e2eb6f04a801836
Jyothi Babu Araja <arajajyothibabu@gmail.com>
2016-04-17 00:11:42 +0530

    trajectory data service updated

--- src/services/TrajectoryDataService.java
+++ src/services/TrajectoryDataService.java
@@ -1,8 +1,29 @@
 package services;
 
+import models.TrajectoryDataModel;
+import utils.OracleDAO;
+
+import java.util.ArrayList;
+
 /**
  * Created by Araja Jyothi Babu on 15-Apr-16.
  */
 public class TrajectoryDataService {
-    //TODO:
+
+    OracleDAO dao;
+
+    public TrajectoryDataService(OracleDAO dao) {
+        this.dao = dao;
+    }
+
+    public ArrayList<String> getAllUniqueDoublets() throws Exception {
+        ArrayList<String> doublets = dao.getAllUniqueDoublets();
+        return doublets;
+    }
+
+    public ArrayList<TrajectoryDataModel> getTrajectoryData(int id) throws Exception {
+        ArrayList<TrajectoryDataModel> trajectoryDataModels = dao.getTrajectoryData(id);
+        return trajectoryDataModels;
+    }
+
 }
